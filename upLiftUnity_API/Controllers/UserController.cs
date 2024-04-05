4f07ad8e4e85ed046a228397823455c1f83c0611
AltinaKasabaqi <altinakasabaqi@gmail.com>
2024-04-05 17:37:50 +0200

    Login Validation + token generation

--- upLiftUnity_API/Controllers/UserController.cs
+++ upLiftUnity_API/Controllers/UserController.cs
@@ -2,12 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using upLiftUnity_API.Models;
 using BCrypt.Net;
+using upLiftUnity_API.Services;
 
 namespace upLiftUnity_API.Controllers
 {
     [ApiController]
     [Route("/api/users")]
-    public class UserController : ControllerBase 
+    public class UserController : ControllerBase
 
     {
         private readonly ILogger<UserController> _logger;
@@ -18,6 +19,28 @@ namespace upLiftUnity_API.Controllers
         {
             _logger = logger;
             _context = _dbcontext;
+        }
+        [HttpPost("/login")]
+        public IActionResult Login([FromForm] UserLogin Request)
+        {
+            var user = _context.Users.FirstOrDefault(user => user.Email == Request.Email);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(Request.Password, user.Password))
+            {
+                return Unauthorized("Mejli ose fjalkalimi eshte gabim");
+            }
+
+            var token = TokenService.GenerateToken(user.Id);
+
+            // Shfaqni vetëm një mesazh në console për tokenin
+            Console.WriteLine("Tokeni i gjeneruar: " + token);
+
+            return Ok();
+        }
+
+        public class UserLogin { 
+            public string Email { get; set;}
+            public string Password { get; set;}
         }
 
         [HttpPost]
