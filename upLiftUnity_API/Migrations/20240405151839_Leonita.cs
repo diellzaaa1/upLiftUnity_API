e9a462d094a4d4ce9d0480b9dc68b62cf3c4e45a
Leonita02 <leonitaa.selmani@gmail.com>
2024-04-12 21:24:21 +0200

    changes for connection to my local db

---
+++ upLiftUnity_API/Migrations/20240405151839_Leonita.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace upLiftUnity_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Leonita : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Calls",
+                columns: table => new
+                {
+                    CallId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CallerNickname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Risk_Level = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calls", x => x.CallId);
+                    table.ForeignKey(
+                        name: "FK_Calls_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rules",
+                columns: table => new
+                {
+                    RuleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RuleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RuleDesc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rules", x => x.RuleId);
+                    table.ForeignKey(
+                        name: "FK_Rules_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calls_UserId",
+                table: "Calls",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rules_UserId",
+                table: "Rules",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calls");
+
+            migrationBuilder.DropTable(
+                name: "Rules");
+        }
+    }
+}
