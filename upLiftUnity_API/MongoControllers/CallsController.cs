bccfef290d57d260fd9607a827443bd89ff2a1fc
AltinaKasabaqi <altinakasabaqi@gmail.com>
2024-05-09 19:13:20 +0200

    Made some changes in the callController

--- upLiftUnity_API/MongoControllers/CallsController.cs
+++ upLiftUnity_API/MongoControllers/CallsController.cs
@@ -15,34 +15,80 @@ namespace upLiftUnity_API.MongoControllers
         }
 
         [HttpGet]
-       public async Task<IEnumerable<Call>> Get()
+        public async Task<IEnumerable<Call>> Get()
         {
             return await _calls.Find(FilterDefinition<Call>.Empty).ToListAsync();
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Call>> GetById(string id)
+        {
+            if (!ObjectId.TryParse(id, out ObjectId objectId))
+            {
+                return BadRequest("Invalid ObjectId format.");
+            }
+
+            var filter = Builders<Call>.Filter.Eq(x => x.Id, objectId);
+            var call = await _calls.Find(filter).FirstOrDefaultAsync();
+
+            return call != null ? Ok(call) : NotFound();
+        }
+
+
 
         [HttpPost]
-        public async Task<ActionResult> Post(Call call)
+        public async Task<ObjectId> Create(Call call)
         {
             await _calls.InsertOneAsync(call);
-            return Ok(call);
+            return (ObjectId)call.Id;
         }
 
-        [HttpPut]
-        public async Task<IActionResult> Update(Call call)
-        {
-            var filter = Builders<Call>.Filter.Eq(x => x.Id, call.Id);
 
-            await _calls.ReplaceOneAsync(filter, call);
-            return Ok(call);
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, Call call)
+        {
+            if (!ObjectId.TryParse(id, out ObjectId objectId))
+            {
+                return BadRequest("Invalid ObjectId format.");
+            }
+
+            // Set the Id property of the call object to the parsed ObjectId
+            call.Id = objectId;
+
+            var filter = Builders<Call>.Filter.Eq(x => x.Id, objectId);
+
+            var update = Builders<Call>.Update
+                .Set(x => x.CallerNickname, call.CallerNickname)
+                .Set(x => x.Description, call.Description)
+                .Set(x => x.RiskLevel, call.RiskLevel);
+
+            var result = await _calls.UpdateOneAsync(filter, update);
+
+            if (result.ModifiedCount > 0)
+            {
+                return Ok(call);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
-        [HttpDelete]
-        public async Task<ActionResult>Delete(ObjectId id)
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
         {
-            var filter = Builders<Call>.Filter.Eq(x => x.Id, id);
+            if (!ObjectId.TryParse(id, out ObjectId objectId))
+            {
+                return BadRequest("Invalid ObjectId format.");
+            }
+
+            var filter = Builders<Call>.Filter.Eq(x => x.Id, objectId);
             await _calls.DeleteOneAsync(filter);
             return Ok();
         }
-       
+
+
     }
 }
--- upLiftUnity_API/MongoModels/Call.cs
+++ upLiftUnity_API/MongoModels/Call.cs
@@ -8,16 +8,16 @@ namespace upLiftUnity_API.MongoModels
 
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)] // Use ObjectId representation for ID
-        public ObjectId Id { get; set; }
+        public ObjectId Id { get; set; } 
 
         [BsonElement("CallerNickname"), BsonRepresentation(BsonType.String)]
-            public string CallerNickname { get; set; }
+        public string CallerNickname { get; set; }
 
-            [BsonRepresentation(BsonType.String)]
-            public string Description { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public string Description { get; set; }
 
-            [BsonRepresentation(BsonType.String)]
-            public int RiskLevel { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public int RiskLevel { get; set; }
             //shto daten e thirrjes veq ne back by default dita e sotit
             //provo vet id me bo autoincrement veq me ni variabel ose me marr thirrjen e fundit ...
         
